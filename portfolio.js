9510803fc8f64cf529c9e43dc4e8d5c53af539da
om1305 <om.agarwal.hero@gmail.com>
2025-06-23 17:52:50 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+let project = document.getElementById("project")
+let skill = document.getElementById("skill")
+let contact_me = document.getElementById("contact_me")
+let navbar = document.getElementById("navbar")
+
+project.addEventListener("click",pro)
+function pro(){
+    
+}
